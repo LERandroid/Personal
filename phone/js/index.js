aab16bda2984a45733a2c365917067514b81c431
LERandroid <3029407778@qq.com>
2026-07-14 22:11:22 +0800

    Update index.js

--- phone/js/index.js
+++ phone/js/index.js
@@ -213,6 +213,6 @@ document.addEventListener('DOMContentLoaded', async function () {
   } catch (err) {
     // 打印完整错误，看控制台精准定位问题
     console.error('加载日记捕获异常：', err);
-    contentEl.innerText = '没有SSL有啥办法呢？点开吧777，来吧，你好呀？！11111111111111111111111111111111111111111111111';
+    contentEl.innerText = '没有SSL有啥办法呢？点开吧777，来吧，你好呀？！';
   }
 });
